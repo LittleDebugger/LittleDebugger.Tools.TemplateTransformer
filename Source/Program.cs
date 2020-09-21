2cc58aae663d3ac566157fabe734ecbe004a2b5c
HedleyGisbourne <hedley.gisbourne@googlemail.com>
2020-09-21 21:15:04 +0200

    Hello world example of net core tool

---
+++ Source/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LittleDebugger.Tools.TemplateTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
