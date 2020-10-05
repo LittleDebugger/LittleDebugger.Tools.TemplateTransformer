cc3c0e1bcf685f0fe0f32fc90998901c8697ab12
littledebugger <hedley.gisbourne@googlemail.com>
2020-10-05 22:08:59 +0200

    Include DragonFruit for command line parser and start to implement recursive directory walk

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.IO;
 
 namespace LittleDebugger.Tools.TemplateTransformer
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string _from;
+        private static string _to;
+
+        static void Main(string from, string to)
         {
-            Console.WriteLine("Hello World!");
+            if (string.IsNullOrEmpty(from))
+            {
+                Console.WriteLine("from agrument is invalid");
+            }
+
+            if (string.IsNullOrEmpty(to))
+            {
+                Console.WriteLine("to agrument is invalid");
+            }
+
+            _from = from;
+            _to = to;
+
+            var directory = Directory.GetCurrentDirectory();
+            HandleDirectory(directory);
         }
+
+        private static void HandleDirectory(string currentDirectory)
+        {
+            Console.WriteLine(currentDirectory);
+            var subdirectories = Directory.GetDirectories(currentDirectory);
+            foreach (var directory in subdirectories)
+            {
+                // TODO rename the directory
+
+                HandleDirectory(currentDirectory);
+            }
+
+            foreach(var file in Directory.GetFiles(currentDirectory))
+            {
+                // TODO rename the filename
+                // TODO check file contents
+            }
+        } 
     }
 }
